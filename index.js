ae94ce9deac39a5781487df0930ab706ffbb7895
aswin567 <[toaswink@gmail.com]>
2021-03-17 12:09:11 +0530

    added cros

--- index.js
+++ index.js
@@ -11,20 +11,22 @@ app.use(bodyParserJSON);
 app.use(bodyParserURLEncoded);
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://aswinkv.com');
-    res.setHeader('Access-Control-Allow-Origin', 'https://aswinkv.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
+    // Website you wish to allow to connect  
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      );
+    // res.setHeader('Access-Control-Allow-Origin', 'http://aswinkv.com');
+    // res.setHeader('Access-Control-Allow-Origin', 'https://aswinkv.com');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
