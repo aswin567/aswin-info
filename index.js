11c4e8532a0a960d5bf19788d6b5e85f2fbf801e
aswin567 <[toaswink@gmail.com]>
2021-03-25 12:55:56 +0530

    error console

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT | 3000;
 var bodyParserJSON = bodyParser.json();
-var bodyParserURLEncoded = bodyParser.urlencoded({extended:true});
+var bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
 app.use(bodyParserJSON);
 app.use(bodyParserURLEncoded);
 app.use(function (req, res, next) {
@@ -17,52 +17,53 @@ app.use(function (req, res, next) {
 });
 app.use(cors())
 app.get('/getPeriod', (req, res) => {
-    
-    try{
-    var m1 = moment('2015-09-07 09:30:00', 'YYYY-MM-DD HH:mm:ss');
-    var m2 = moment(new Date(), 'YYYY-MM-DD HH:mm:ss');
-    res.send(moment.preciseDiff(m1, m2, true))
+
+    try {
+        var m1 = moment('2015-09-07 09:30:00', 'YYYY-MM-DD HH:mm:ss');
+        var m2 = moment(new Date(), 'YYYY-MM-DD HH:mm:ss');
+        res.send(moment.preciseDiff(m1, m2, true))
     }
-    catch(err){
-        res.send(err);
+    catch (err) {
+        res.json(err);
     }
 })
 
 
 app.post('/sendMail', (req, res, next) => {
-    console.log(req.body)
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'aswinkv.dev@gmail.com',//replace with your email
-            pass: 'developer!@#123'//replace with your password
-        }
-    });
-    var mailOptions = {
-        from: 'aswinkv.dev@gmail.com',//replace with your email
-        to: ['mykrishna16@gmail.com', 'admin@aswinkv.com'],//replace with your email
-        subject: req.body.subject,
-        html: `<h2> name:${req.body.name} </h2>
+    try {
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'aswinkv.dev@gmail.com',//replace with your email
+                pass: 'developer!@#123'//replace with your password
+            }
+        });
+        var mailOptions = {
+            from: 'aswinkv.dev@gmail.com',//replace with your email
+            to: ['mykrishna16@gmail.com', 'admin@aswinkv.com'],//replace with your email
+            subject: req.body.subject,
+            html: `<h2> name:${req.body.name} </h2>
                 <h3> email:${req.body.email} </h3>
                 <h3> phonenumber:${req.body.mobile} </h3><br><hr>
                 <p>${req.body.content} </p>`
-    };
-    /*
-     Here comes the important part, sendMail is the method which actually sends email, it takes mail options and
-    call back as parameter
-    */
-    transporter.
-    sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.send('error') // if error occurs send error as response to client
-        }
-        else {
-            console.log('Email sent: ' + info.response);
-            res.send('Sent Successfully')//if mail is sent successfully send Sent successfully as response
-        }
-    });
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                res.json(error) // if error occurs send error as response to client
+            }
+            else {
+                console.log('Email sent: ' + info.response);
+                res.send('Sent Successfully')//if mail is sent successfully send Sent successfully as response
+            }
+        });
+    }
+    catch (err) {
+
+        res.json(err);
+    }
+
 })
-app.listen(port, () => {})
+app.listen(port, () => { })
 
 
